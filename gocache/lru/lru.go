367efbcb5a67b7d731dba1699d7040a9280ec73f
ubuntu <863337056@qq.com>
2024-07-03 01:19:06 -0700

    实现缓存过期

--- gocache/lru/lru.go
+++ gocache/lru/lru.go
@@ -1,6 +1,11 @@
 package lru
 
-import "container/list"
+import (
+	"container/list"
+	"time"
+)
+
+type NowFunc func() time.Time
 
 // Cache is a LRU cache. It is not safe for concurrnet access
 type Cache struct {
@@ -10,12 +15,18 @@ type Cache struct {
 	//list.Element是container/list包中的一个结构体，用于表示双向链表中的一个元素
 	cache     map[string]*list.Element      // 一个字符串到list.Element的映射，，键是字符串，值是双向链表中对应节点的指针
 	OnEvicted func(key string, value Value) // optional and executed when an entry is purged.回调函数
+
+	// Now is the Now() function the cache will use to determine
+	// the current time which is used to calculate expired values
+	// Defaults to time.Now()
+	Now NowFunc
 }
 
-//在链表中仍保存每个值对应的 key 的好处在于，淘汰队首节点时，需要用 key 从字典中删除对应的映射。
+//在链表中仍保存每个值对应的 key x的好处在于，淘汰队首节点时，需要用 key 从字典中删除对应的映射。
 type entry struct { //双向链表的数据类型
 	key   string
 	value Value
+	expire time.Time
 }
 
 // Value use Len to count how many bytes it 
@@ -31,6 +42,7 @@ func New(maxBytes int64, onEvicted func(string, Value)) *Cache {
 		ll:        list.New(),
 		cache:     make(map[string]*list.Element),
 		OnEvicted: onEvicted,
+		Now:       time.Now,
 	}
 }
 
@@ -38,42 +50,75 @@ func New(maxBytes int64, onEvicted func(string, Value)) *Cache {
 //get look ups a key's value
 //如果键对应的链表节点存在，则将对应节点移动到头部，并返回查找到的值。
 func (c *Cache) Get(key string) (value Value,ok bool) {
+	if c.cache == nil {
+		return
+	}
 	if ele,ok := c.cache[key];ok { //找到后ele是指向list.Element的指针
-		c.ll.MoveToFront(ele)     //约定front是头部
+	
 		//结构体Element中有名为Value的字段，类型是interface{},意味着其可以存储任意类型的值 ele.Value 存储了与该元素相关联的数据。
 		//使用类型断言，将interface{}类型的ele.Value转换为具体的*entry类型便能，够访问 entry 结构体的 key 和 value 字段。
 		kv := ele.Value.(*entry)   
+		// If the entry has expired, remove it from the cache
+		if !kv.expire.IsZero() && kv.expire.Before(c.Now()) {
+			c.removeElement(ele)
+			return nil,false
+		}
+		c.ll.MoveToFront(ele)     //约定front是头部
 		return kv.value,true
 	}
 	return
 }
 
+func (c *Cache) Remove(key string) {
+	if c.cache == nil {
+		return
+	}
+	if ele,ok := c.cache[key];ok {
+		c.removeElement(ele)
+	}
+}
+
 // RemoveOldest removes the oldest item
 func (c *Cache) RemoveOldest() {
+	if c.cache == nil {
+		return
+	}
 	//取到末尾节点，从链表中删除
 	ele := c.ll.Back()
 	if ele != nil {
-		c.ll.Remove(ele)
-		kv := ele.Value.(*entry)
-		delete(c.cache, kv.key)
-		c.nbytes -= int64(len(kv.key)) + int64(kv.value.Len())
-		if c.OnEvicted != nil {
-			c.OnEvicted(kv.key, kv.value)
-		}
+		c.removeElement(ele)
+	}
+}
+
+func (c *Cache) removeElement(ele *list.Element) {
+	c.ll.Remove(ele)
+	kv := ele.Value.(*entry)
+	delete(c.cache,kv.key)
+	c.nbytes -= int64(len(kv.key)) + int64(kv.value.Len())
+	if c.OnEvicted != nil {
+		c.OnEvicted(kv.key,kv.value)
 	}
 }
 
 
 // Add adds a value to the cache.
-func (c *Cache) Add(key string,value Value) {
+func (c *Cache) Add(key string,value Value,expire time.Time) {
+	if c.cache == nil {
+		c.cache = make(map[string]*list.Element)
+		c.ll = list.New()
+	}
 	//如果键存在，则更新对应节点的值，并将该节点移到头部。
 	if ele, ok := c.cache[key];ok {
-		c.ll.MoveToFront(ele)
 		kv := ele.Value.(*entry)
+		if c.OnEvicted != nil {
+			c.OnEvicted(key,kv.value)
+		}
+		c.ll.MoveToFront(ele)
+		kv.expire = expire
 		c.nbytes += int64(value.Len()) - int64(kv.value.Len())
 		kv.value = value
 	} else { //不存在则是新增场景，首先头部添加新节点 &entry{key, value}, 并字典中添加 key 和节点的映射关系。
-		ele := c.ll.PushFront(&entry{key, value})
+		ele := c.ll.PushFront(&entry{key, value,expire})
 		c.cache[key] = ele
 		c.nbytes += int64(len(key)) + int64(value.Len())
 	}//更新 c.nbytes，如果超过了设定的最大值 c.maxBytes，则移除最少访问的节点。
@@ -84,5 +129,20 @@ func (c *Cache) Add(key string,value Value) {
 
 // Len the number of cache entries
 func (c *Cache) Len() int {
+	if c.cache == nil {
+		return 0
+	}
 	return c.ll.Len()
+}
+
+// Clear purges all stored items from the cache.
+func (c *Cache) Clear() {
+	if c.OnEvicted != nil {
+		for _, e := range c.cache {
+			kv := e.Value.(*entry)
+			c.OnEvicted(kv.key, kv.value)
+		}
+	}
+	c.ll = nil
+	c.cache = nil
 }
--- gocache/lru/lru_test.go
+++ gocache/lru/lru_test.go
@@ -3,6 +3,7 @@ package lru
 import (
 	"reflect"
 	"testing"
+	"time"
 )
 
 type String string
@@ -13,7 +14,7 @@ func (d String) Len() int {
 
 func TestGet(t *testing.T) {
 	lru := New(int64(0), nil)
-	lru.Add("key1", String("1234"))
+	lru.Add("key1", String("1234"),time.Time{})
 	if v, ok := lru.Get("key1"); !ok || string(v.(String)) != "1234" {
 		t.Fatalf("cache hit key1=1234 failed")
 	}
@@ -27,9 +28,9 @@ func TestRemoveoldest(t *testing.T) {
 	v1, v2, v3 := "value1", "value2", "v3"
 	cap := len(k1 + k2 + v1 + v2)
 	lru := New(int64(cap), nil)
-	lru.Add(k1, String(v1))
-	lru.Add(k2, String(v2))
-	lru.Add(k3, String(v3))
+	lru.Add(k1, String(v1),time.Time{})
+	lru.Add(k2, String(v2),time.Time{})
+	lru.Add(k3, String(v3),time.Time{})
 
 	if _, ok := lru.Get("key1"); ok || lru.Len() != 2 {
 		t.Fatalf("Removeoldest key1 failed")
@@ -42,10 +43,10 @@ func TestOnEvicted(t *testing.T) {
 		keys = append(keys, key)
 	}
 	lru := New(int64(10), callback)
-	lru.Add("key1", String("123456"))
-	lru.Add("k2", String("k2"))
-	lru.Add("k3", String("k3"))
-	lru.Add("k4", String("k4"))
+	lru.Add("key1", String("123456"),time.Time{})
+	lru.Add("k2", String("k2"),time.Time{})
+	lru.Add("k3", String("k3"),time.Time{})
+	lru.Add("k4", String("k4"),time.Time{})
 
 	expect := []string{"key1", "k2"}
 
@@ -56,10 +57,38 @@ func TestOnEvicted(t *testing.T) {
 
 func TestAdd(t *testing.T) {
 	lru := New(int64(0), nil)
-	lru.Add("key", String("1"))
-	lru.Add("key", String("111"))
+	lru.Add("key", String("1"),time.Time{})
+	lru.Add("key", String("111"),time.Time{})
 
 	if lru.nbytes != int64(len("key")+len("111")) {
 		t.Fatal("expected 6 but got", lru.nbytes)
 	}
+}
+
+func TestExpire(t *testing.T) {
+	var tests = []struct {
+		name       string
+		key        string
+		expectedOk bool
+		expire     time.Duration
+		wait       time.Duration
+	}{
+		{"not-expired", "myKey", true, time.Second * 1, time.Duration(0)},
+		{"expired", "expiredKey", false, time.Millisecond * 100, time.Millisecond * 150},
+	}
+
+	for _, tt := range tests {
+		lru := New(int64(0), nil)
+		lru.Add("key", String("123456"), time.Now().Add(tt.expire))
+		time.Sleep(tt.wait)
+		val, ok := lru.Get("key")
+		if ok != tt.expectedOk {
+			t.Fatalf("%s: cache hit = %v; want %v", tt.name, ok, !ok)
+		} else if ok {
+            retrievedVal := val.(String)  // 使用类型断言来获取 String 值
+            if string(retrievedVal) != "123456" {
+                t.Fatalf("%s expected get to return 123456 but got %v", tt.name, retrievedVal)
+            }
+        }
+    }
 }
