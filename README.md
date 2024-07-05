792759cec3d57b4ce87d41ab88b99567c756ad5d
ubuntu <863337056@qq.com>
2024-07-05 05:00:13 -0700

    增加新内存算法

--- README.md
+++ README.md
@@ -64,15 +64,44 @@
 ├── README.md
 └── run.sh
 ```
+第三版：
+增加了缓存过期时间,增加lfu算法,增加了arc算法，lru-K算法、
+根据过期时间懒汉式删除过期数据,也可主动刷新过期缓存
+现已支持内存算法:
+lru、lfu、arc、hashlru、hashlfu
 
-安装命令： 
+性能对比
+hashlru 与 lru 性能对比
 
-go get github.com/golang/protobuf/proto 
+算法	耗时
+lru	220.2s
+hashlru-2分区	267.75s
+hashlru-4分区	137.36s
+hashlru-8分区	22.4s
+hashlru-16分区	23.57s
+hashlru-32分区	16.84s
+hashlru-64分区	15.29s
 
-go get google.golang.org/protobuf/reflect/protoreflect 
+hashlfu 与 lfu 性能对比
 
-go get google.golang.org/protobuf/runtime/protoimpl 
+算法	耗时
+lru	220.92s
+hashlfu-2分区	231.28s
+hashlfu-4分区	72.74s
+hashlfu-8分区	20.33s
+hashlfu-16分区	17.76s
+hashlfu-32分区	16.93s
+hashlfu-64分区	16.03s
 
-go mod init xxx
+hash算法减少耗时原因:
+LruCache在高QPS下的耗时增加原因分析：
+
+线程安全的LruCache中有锁的存在。每次读写操作之前都有加锁操作，完成读写操作之后还有解锁操作。 在低QPS下，锁竞争的耗时基本可以忽略；但是在高QPS下，大量的时间消耗在了等待锁的操作上，导致耗时增长。
+
+HashLruCache适应高QPS场景：
+
+针对大量的同步等待操作导致耗时增加的情况，解决方案就是尽量减小临界区。引入Hash机制，对全量数据做分片处理，在原有LruCache的基础上形成HashLruCache，以降低查询耗时。
+
+HashLruCache引入哈希算法，将缓存数据分散到N个LruCache上。查询时也按照相同的哈希算法，先获取数据可能存在的分片，然后再去对应的分片上查询数据。这样可以增加LruCache的读写操作的并行度，减小同步等待的耗时。
 
 go mod tidy
---
+++ gocache/simplelfu/lfu.go
@@ -0,0 +1,258 @@
+package simplelfu
+
+import (
+	"container/list"
+	"errors"
+	"math"
+	"time"
+)
+// EvictCallback is used to get a callback when a cache entry is evicted
+// EvictCallback 用于在缓存条目被淘汰时的回调函数
+type EvictCallback func(key interface{}, value interface{}, expirationTime int64)
+
+// LFU implements a non-thread safe fixed size LFU cache
+// LFU 实现一个非线程安全的固定大小的LFU缓存
+type LFU struct {
+	size      int
+	evictList *list.List
+	items     map[interface{}]*list.Element
+	onEvict   EvictCallback
+}
+
+// entry is used to hold a value in the evictList
+// 缓存详细信息
+type entry struct {
+	key            interface{}
+	value          interface{}
+	weight         int64 // 访问次数
+	expirationTime int64
+}
+
+// NewLFU constructs an LFU of the given size
+// NewLFU 构造一个给定大小的LFU
+func NewLFU(size int, onEvict EvictCallback) (*LFU, error) {
+	if size <= 0 {
+		return nil, errors.New("must provide a positive size")
+	}
+	c := &LFU{
+		size:      size,
+		evictList: list.New(),
+		items:     make(map[interface{}]*list.Element),
+		onEvict:   onEvict,
+	}
+	return c, nil
+}
+
+// Purge is used to completely clear the cache.
+// Purge 用于完全清除缓存
+func (c *LFU) Purge() {
+	for k, v := range c.items {
+		if c.onEvict != nil {
+			c.onEvict(k, v.Value.(*entry).value, v.Value.(*entry).expirationTime)
+		}
+		delete(c.items, k)
+	}
+	c.evictList.Init()
+}
+
+// PurgeOverdue is used to completely clear the overdue cache.
+// PurgeOverdue 清除过期缓存
+func (c *LFU) PurgeOverdue() {
+	for _, ent := range c.items {
+		// 判断此值是否已经超时,如果超时则进行删除
+		if checkExpirationTime(ent.Value.(*entry).expirationTime) {
+			c.removeElement(ent)
+		}
+	}
+	c.evictList.Init()
+}
+
+// Add adds a value to the cache.  Returns true if an eviction occurred.
+// Add 向缓存添加一个值。如果已经存在,则更新信息
+func (c *LFU) Add(key, value interface{}, expirationTime int64) (ok bool) {
+	// 判断缓存中是否已经存在数据,如果已经存在则更新数据
+	if ent, ok := c.items[key]; ok {
+		ent.Value.(*entry).value = value
+		ent.Value.(*entry).expirationTime = expirationTime
+		ent.Value.(*entry).weight++
+		// 判断前一个元素 weight 值是否小于当前元素, 如果小于则替换顺序
+		if (ent.Prev() != nil) && (ent.Prev().Value.(*entry).weight < ent.Value.(*entry).weight) {
+			c.evictList.MoveBefore(ent, ent.Prev())
+		}
+		return true
+	}
+	// 判断缓存条数是否已经达到限制
+	if c.evictList.Len() >= c.size {
+		c.removeOldest()
+	}
+	// 创建数据
+	ent := &entry{key, value, 1, expirationTime}
+	c.items[key] = c.evictList.PushBack(ent)
+
+	return true
+}
+
+// Get looks up a key's value from the cache.
+// Get 从缓存中查找一个键的值。
+func (c *LFU) Get(key interface{}) (value interface{}, expirationTime int64, ok bool) {
+	// 判断缓存是否存在
+	if ent, ok := c.items[key]; ok {
+		// 判断此值是否已经超时,如果超时则进行删除
+		if checkExpirationTime(ent.Value.(*entry).expirationTime) {
+			c.removeElement(ent)
+			return nil, 0, false
+		}
+		ent.Value.(*entry).weight++
+		// 判断前一个元素 weight 值是否小于当前元素, 如果小于则替换顺序
+		if (ent.Prev() != nil) && (ent.Prev().Value.(*entry).weight < ent.Value.(*entry).weight) {
+			c.evictList.MoveBefore(ent, ent.Prev())
+		}
+		return ent.Value.(*entry).value, ent.Value.(*entry).expirationTime, true
+	}
+	return nil, 0, false
+}
+
+// Contains checks if a key is in the cache, without updating the recent-ness
+// or deleting it for being stale.
+// Contains 检查某个键是否在缓存中，但不更新缓存的状态
+func (c *LFU) Contains(key interface{}) (ok bool) {
+	ent, ok := c.items[key]
+	if ok {
+		// 判断此值是否已经超时,如果超时则进行删除
+		if checkExpirationTime(ent.Value.(*entry).expirationTime) {
+			c.removeElement(ent)
+			return !ok
+		}
+	}
+	return ok
+}
+
+// Peek returns the key value (or undefined if not found) without updating
+// the "recently used"-ness of the key.
+// Peek 在不更新的情况下返回键值(如果没有找到则返回false),不更新缓存的状态
+func (c *LFU) Peek(key interface{}) (value interface{}, expirationTime int64, ok bool) {
+	var ent *list.Element
+	if ent, ok = c.items[key]; ok {
+		// 判断是否已经超时
+		if checkExpirationTime(ent.Value.(*entry).expirationTime) {
+			c.removeElement(ent)
+			return nil, 0, ok
+		}
+		return ent.Value.(*entry).value, ent.Value.(*entry).expirationTime, true
+	}
+	return nil, 0, ok
+}
+
+// Remove removes the provided key from the cache, returning if the
+// key was contained.
+// Remove 从缓存中移除提供的键
+func (c *LFU) Remove(key interface{}) (ok bool) {
+	if ent, ok := c.items[key]; ok {
+		c.removeElement(ent)
+		return ok
+	}
+	return ok
+}
+
+// RemoveOldest removes the oldest item from the cache.
+// RemoveOldest 从缓存中移除最老的项
+func (c *LFU) RemoveOldest() (key interface{}, value interface{}, expirationTime int64, ok bool) {
+	if ent := c.evictList.Back(); ent != nil {
+		// 判断是否已经超时
+		if checkExpirationTime(ent.Value.(*entry).expirationTime) {
+			c.removeElement(ent)
+			return c.RemoveOldest()
+		}
+		c.removeElement(ent)
+
+		return ent.Value.(*entry).key, ent.Value.(*entry).value, ent.Value.(*entry).expirationTime, true
+	}
+	return nil, nil, 0, false
+}
+
+// GetOldest returns the oldest entry
+// GetOldest 返回最老的条目
+func (c *LFU) GetOldest() (key interface{}, value interface{}, expirationTime int64, ok bool) {
+	ent := c.evictList.Back()
+	if ent != nil {
+		// 判断此值是否已经超时,如果超时则进行删除
+		if checkExpirationTime(ent.Value.(*entry).expirationTime) {
+			c.removeElement(ent)
+			return c.GetOldest()
+		}
+		// 引用自增
+		ent.Value.(*entry).weight++
+		return ent.Value.(*entry).key, ent.Value.(*entry).value, ent.Value.(*entry).expirationTime, true
+	}
+	return nil, nil, 0, false
+}
+
+// Keys returns a slice of the keys in the cache, from oldest to newest.
+// Keys 返回缓存的切片，从最老的到最新的。
+func (c *LFU) Keys() []interface{} {
+	keys := make([]interface{}, len(c.items))
+	i := 0
+	for ent := c.evictList.Back(); ent != nil; ent = ent.Prev() {
+		keys[i] = ent.Value.(*entry).key
+		i++
+	}
+	return keys
+}
+
+// Len returns the number of items in the cache.
+// Len 返回缓存中的条数
+func (c *LFU) Len() int {
+	return c.evictList.Len()
+}
+
+// Resize changes the cache size.
+// Resize 改变缓存大小。
+func (c *LFU) Resize(size int) (evicted int) {
+	diff := c.Len() - size
+	if diff < 0 {
+		diff = 0
+	}
+	for i := 0; i < diff; i++ {
+		c.removeOldest()
+	}
+	c.size = size
+	return diff
+}
+
+// ResizeWeight changes the cache eight weight size.
+// ResizeWeight 改变缓存中Weight大小。
+func (c *LFU) ResizeWeight(percentage int) {
+	if percentage > 0 || percentage < 100 {
+		for ent := c.evictList.Back(); ent != nil; ent = ent.Prev() {
+			ent.Value.(*entry).weight = int64(math.Ceil(float64(ent.Value.(*entry).weight / 100 * int64(percentage))))
+		}
+	}
+}
+
+// removeOldest removes the oldest item from the cache.
+// removeOldest 从缓存中移除最老的项。
+func (c *LFU) removeOldest() {
+	ent := c.evictList.Back()
+	if ent != nil {
+		c.removeElement(ent)
+	}
+}
+
+// removeElement is used to remove a given list element from the cache
+// removeElement 从缓存中移除一个列表元素
+func (c *LFU) removeElement(e *list.Element) {
+	c.evictList.Remove(e)
+	delete(c.items, e.Value.(*entry).key)
+	if c.onEvict != nil {
+		c.onEvict(e.Value.(*entry).key, e.Value.(*entry).value, e.Value.(*entry).expirationTime)
+	}
+}
+
+// checkExpirationTime is Determine if the cache has expired
+// checkExpirationTime 判断缓存是否已经过期
+func checkExpirationTime(expirationTime int64) (ok bool) {
+	if 0 != expirationTime && expirationTime <= time.Now().UnixNano()/1e6 {
+		return true
+	}
+	return false
+}
